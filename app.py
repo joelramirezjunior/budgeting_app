108ba9ed047e39dbcdfec86e069fa58a097e1fb2
Joel Ramirez <46511700+joelramirezjunior@users.noreply.github.com>
2023-09-23 22:52:17 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# More routes to interact with the database...
