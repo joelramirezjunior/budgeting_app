ce33b9b050ec09db6694016ade09d67a2cb8484d
Joel Ramirez <46511700+joelramirezjunior@users.noreply.github.com>
2023-09-23 23:02:01 -0700

    First Commit! Developing back-end.

--- app.py
+++ app.py
@@ -10,4 +10,27 @@ mongo = PyMongo(app)
 def hello_world():
     return 'Hello, World!'
 
-# More routes to interact with the database...
+
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    transactions = mongo.db.transactions
+    # Parse transaction details from request
+    user_id = request.json.get('user_id')
+    category = request.json.get('category')
+    amount = request.json.get('amount')
+    date = request.json.get('date')
+    description = request.json.get('description')
+    
+    # Insert to the database
+    transaction_id = transactions.insert({
+        'user_id': user_id,
+        'category': category,
+        'amount': amount,
+        'date': date,
+        'description': description
+    })
+    
+    return jsonify({'transaction_id': str(transaction_id)})
+
+
+app.run(port=5000)
